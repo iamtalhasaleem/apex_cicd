e074d4d646ee310bb43942c9c473e48ba3994a10
iamtalhasaleem <iamtalhasaleem911@gmail.com>
2026-05-07 11:07:25 +0000

    cicd10 page

---
+++ apex/f103/application/pages/page_00004.sql
@@ -0,0 +1,26 @@
+prompt --application/pages/page_00004
+begin
+--   Manifest
+--     PAGE: 00004
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2024.11.30'
+,p_release=>'24.2.0'
+,p_default_workspace_id=>32147286929531095
+,p_default_application_id=>103
+,p_default_id_offset=>0
+,p_default_owner=>'MAXPRINT_DEMO'
+);
+wwv_flow_imp_page.create_page(
+ p_id=>4
+,p_name=>'cicd10-page'
+,p_alias=>'CICD10-PAGE'
+,p_step_title=>'cicd10-page'
+,p_autocomplete_on_off=>'OFF'
+,p_page_template_options=>'#DEFAULT#'
+,p_protection_level=>'C'
+,p_page_component_map=>'11'
+);
+wwv_flow_imp.component_end;
+end;
+/
--- apex/f103/application/shared_components/navigation/lists/navigation_menu.sql
+++ apex/f103/application/shared_components/navigation/lists/navigation_menu.sql
@@ -15,7 +15,7 @@ wwv_flow_imp_shared.create_list(
  p_id=>wwv_flow_imp.id(32957555579992020)
 ,p_name=>'Navigation Menu'
 ,p_list_status=>'PUBLIC'
-,p_version_scn=>32811771
+,p_version_scn=>61708172
 );
 wwv_flow_imp_shared.create_list_item(
  p_id=>wwv_flow_imp.id(32969307732992082)
@@ -33,6 +33,15 @@ wwv_flow_imp_shared.create_list_item(
 ,p_list_item_icon=>'fa-forms'
 ,p_list_item_current_type=>'COLON_DELIMITED_PAGE_LIST'
 ,p_list_item_current_for_pages=>'3'
+);
+wwv_flow_imp_shared.create_list_item(
+ p_id=>wwv_flow_imp.id(68347272422522988)
+,p_list_item_display_sequence=>30
+,p_list_item_link_text=>'cicd10-page'
+,p_list_item_link_target=>'f?p=&APP_ID.:4:&APP_SESSION.::&DEBUG.:::'
+,p_list_item_icon=>'fa-file-o'
+,p_list_item_current_type=>'COLON_DELIMITED_PAGE_LIST'
+,p_list_item_current_for_pages=>'4'
 );
 wwv_flow_imp.component_end;
 end;
